b6cf956f1686c90a5ec1ae8d58ce4d2377a7882d
Anshikabharadwaj <Anshikabharadwaj32@gmail.com>
2025-05-13 15:22:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function appendvalue(value) {
+    document.getElementById("inp").value += value;
+}
+
+function result() {
+    let result = eval(document.getElementById("inp").value);
+    document.getElementById("inp").value = result;
+}
+
+
+function clearScreen() {
+    console.log("it was calling...")
+    document.getElementById("inp").value = "";
+}
+
+function deleteLastChar() {
+    const display = document.getElementById("inp");
+    display.value = display.value.slice(0, -1);
+}
